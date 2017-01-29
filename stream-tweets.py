7bc0ca0beb6bb75df81b5757a034663ff762ae38
prav10194 <prav10194@gmail.com>
2017-01-29 16:39:08 -0600

    Streaming file updated in repository

--- stream-tweets.py
+++ stream-tweets.py
@@ -4,10 +4,10 @@ from tweepy.streaming import StreamListener
 
 
 #consumer key, consumer secret, access token, access secret.
-consumer_key = "xJ3bfmVFkMHY0zegrQfxC7Qtu"
-consumer_secret = "GonIvFlmBdyvluwqxSqcRdP5SrqY02dFnbzQA3P28I9F5SxXj6"
-access_key = "2413044493-18vqGOAPbHZBmDi5SXLtnfC3VwpbUJ0sVRBG6PN"
-access_secret = "NuUnbuHVbPHJ4boCENtVdeavBB7PEyCxFzCi1RYHK2awR"
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
 
 class listener(StreamListener):
 
